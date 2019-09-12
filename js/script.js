a7bce36f977d575a1d706ec62a6c562f7701c4a2
19uday <16co149.uday@nitk.edu.in>
2019-09-12 15:48:39 +0530

    New files added

--- js/script.js
+++ js/script.js
@@ -3,30 +3,65 @@
 
     $(document).ready(function() {
         $("#recon").on("click", function() {
+            if($("#recon").hasClass("active") === false){
+                $("#recon").addClass("active")
+            }
+            if($("#escrow").hasClass("active") === true){
+                $("#escrow").removeClass("active")
+            }
+            if($("#bcm").hasClass("active") === true){
+                $("#bcm").removeClass("active")
+            }
+        });
+
+        $("#bcm").on("click", function() {
+            if($("#bcm").hasClass("active") === false){
+                $("#bcm").addClass("active")
+            }
+            if($("#escrow").hasClass("active") === true){
+                $("#escrow").removeClass("active")
+            }
+            if($("#recon").hasClass("active") === true){
+                $("#recon").removeClass("active")
+            }
+        });
+
+        $("#escrow").on("click", function() {
+            if($("#escrow").hasClass("active") === false){
+                $("#escrow").addClass("active")
+            }
+            if($("#recon").hasClass("active") === true){
+                $("#recon").removeClass("active")
+            }
+            if($("#bcm").hasClass("active") === true){
+                $("#bcm").removeClass("active")
+            }
+        });
+        $("#recon").on("click", function() {
             $(".section1").animateAlert('slideOutUp', 6000);
         });
         $("#close-alert-box-news").on("click", function() {
             $("#alert-box-news").animateAlert('slideOutUp', 500, true);
         });
          $('.owl-carousel').owlCarousel({
-                loop:true,
-                margin:10,
-                items : 4,
-                navigation: true,
-                nav:true,
-                navigationText : ["<i class='fa fa-long-arrow-left'></i>","<i class='fa fa-long-arrow-right'></i>"],
-                responsive:{
-                    0:{
-                        items:1
-                    },
-                    600:{
-                        items:3
-                    },
-                    1000:{
-                        items:5
-                    }
-                }
-            });
+    loop:true,
+    margin:10,
+    items : 4,
+    navigation: true,
+    nav:true,
+    navigationText : ["<i class='fa fa-long-arrow-left'></i>","<i class='fa fa-long-arrow-right'></i>"],
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+});
     
     
           $('#b').hide();
@@ -63,3 +98,4 @@
     });
 });
     });
+
