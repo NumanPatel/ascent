dbd1725369d5291468b57b80fd6dcc5318cb6bbf
19uday <16co149.uday@nitk.edu.in>
2019-09-18 05:09:02 +0530

    Final changes to first website

--- js/script.js
+++ js/script.js
@@ -101,10 +101,10 @@
    });
 
     $('.count').each(function () {
-    $(this).prop('Counter',0).animate({
+    $(this).prop('Counter',100).animate({
         Counter: $(this).text()
     }, {
-        duration: 12000,
+        duration: 1200000,
         easing: 'swing',
         step: function (now) {
             $(this).text(Math.ceil(now));
